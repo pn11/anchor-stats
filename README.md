a814fea8444c650b06cb2a4e91660e0c0f0f4fb6
oka <pn11@users.noreply.github.com>
2020-12-23 18:59:51 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Anchor Stats
+
+Automatically retrieve episode stats using Selenium.
+
+## Requirements
+
+Download chromedriver from <https://chromedriver.chromium.org/downloads>
---
+++ run.py
@@ -0,0 +1,64 @@
+import json
+import time
+import os
+
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+
+
+# EMAIL, PASSWORD は .env に書く
+load_dotenv(verbose=True)
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+EMAIL = os.environ.get("EMAIL")
+PASSWORD = os.environ.get("PASSWORD")
+
+chromeOptions = webdriver.ChromeOptions()
+prefs = {"download.default_directory" : os.path.dirname(__file__)}
+chromeOptions.add_experimental_option("prefs", prefs)
+
+driver = webdriver.Chrome(chrome_options=chromeOptions)
+
+driver.get('https://anchor.fm/dashboard/episodes')
+
+def login():
+    username = driver.find_element_by_name('email')
+    username.send_keys(EMAIL)
+
+    password = driver.find_element_by_name('password')
+    password.send_keys(PASSWORD)
+    time.sleep(1)
+
+    button = driver.find_element_by_xpath('//*[@id="LoginForm"]/div[3]/button')
+    
+    button.click()
+
+def find_episodes():
+    a_elements = driver.find_elements_by_xpath('//a[@data-cy="episodeRowLink"]')
+    links = [x.get_attribute('href') for x in a_elements]
+    print(f"{len(a_elements)} episodes found.")
+    return links
+    
+
+def download_stats(link):
+    driver.get(link)
+    time.sleep(10)
+    driver.find_element_by_xpath('//*[@id="app-content"]/div/div/div/div[2]/div/div[2]/div/div/div/div/div/div').click()
+    time.sleep(0.5)
+    driver.find_element_by_xpath('//*[@id="app-content"]/div/div/div/div[2]/div/div[2]/div/div/div/div[2]/div/div[1]/div[6]/div').click()
+    time.sleep(0.5)
+    driver.find_element_by_xpath('//*[@id="app-content"]/div/div/div/div[3]/div[1]/div/div/div/div/div/div').click()
+    time.sleep(0.5)
+    driver.find_element_by_xpath('//*[@id="app-content"]/div/div/div/div[3]/div[1]/div/div/div/div[2]/div/div/div[1]/div/div').click()
+    time.sleep(0.5)
+    driver.find_element_by_xpath('//*[@id="app-content"]/div/div/div/div[3]/div[3]/div/div/div/a').click()
+    time.sleep(5)
+
+
+login()
+time.sleep(10)
+links = find_episodes()
+print(links)
+for l in links:
+    download_stats(l)
